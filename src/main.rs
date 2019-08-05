dc94f1209710f2045d5e2dccf1f51e3e8794e5ea
Martin Ivicic <martin.ivicic@pan-net.eu>
2019-08-05 17:11:20 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use std::path::PathBuf;
+use std::process::exit;
 
 use structopt::StructOpt;
 
@@ -19,7 +20,18 @@ use streams::{InputStream, OutputStream};
 pub use vars::Vars;
 use vars::{EmptyVars, JsonVars, VarsError, VarsFormat, YamlVars};
 
-fn cli_args<'a>(stdin: &'a io::Stdin, stdout: &'a io::Stdout) -> Result<(InputStream<'a>, OutputStream<'a>, Box<dyn Vars>, Option<PathBuf>), Error<'static>> {
+fn cli_args<'a>(
+    stdin: &'a io::Stdin,
+    stdout: &'a io::Stdout,
+) -> Result<
+    (
+        InputStream<'a>,
+        OutputStream<'a>,
+        Box<dyn Vars>,
+        Option<PathBuf>,
+    ),
+    Error<'a>,
+> {
     let opt = cli::Opt::from_args();
 
     let input = if let Some(input_file) = opt.input_file {
@@ -53,11 +65,8 @@ fn cli_args<'a>(stdin: &'a io::Stdin, stdout: &'a io::Stdout) -> Result<(InputSt
     Ok((input, output, vars, opt.vars_file))
 }
 
-
-fn main() -> Result<(), Error<'static>> {
-    let stdin = io::stdin();
-    let stdout = io::stdout();
-    let (mut input, mut output, vars, vars_file) = cli_args(&stdin, &stdout)?;
+fn real_main<'a>(stdin: &'a io::Stdin, stdout: &'a io::Stdout) -> Result<(), Error<'a>> {
+    let (mut input, mut output, vars, vars_file) = cli_args(stdin, stdout)?;
 
     let mut input_chars = PositionIterator::from(input.chars());
 
@@ -67,18 +76,25 @@ fn main() -> Result<(), Error<'static>> {
             input_chars.line(),
             input_chars.col(),
             match err {
-                TranslateError::Input(io_err) => Error::Input(input, io_err),
+                TranslateError::Input(io_err) => return Err(Error::Input(input, io_err)),
                 TranslateError::Output(io_err) => Error::Output(output, io_err),
                 TranslateError::Expr(ExprError::Output(io_err)) => Error::Output(output, io_err),
                 TranslateError::Expr(ExprError::Input(io_err)) => Error::Input(input, io_err),
-                TranslateError::Expr(ExprError::Vars(vars_err)) => {
-                    Error::Vars(vars_file, vars_err)
-                }
+                TranslateError::Expr(ExprError::Vars(vars_err)) => Error::Vars(vars_file, vars_err),
                 TranslateError::Expr(ExprError::Internal(expr_err)) => Error::Expr(expr_err),
                 TranslateError::Vars(expr_err) => Error::Vars(vars_file, expr_err),
             },
         );
-        std::process::exit(1);
+        exit(1);
     }
     Ok(())
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let stdout = io::stdout();
+    if let Err(err) = real_main(&stdin, &stdout) {
+        eprintln!("{}", err);
+        exit(1);
+    };
 }
